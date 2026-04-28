7c6be26ee2e32607229e301e03dc1311fdd382e7
ErnestC77 <122796474+ErnestC77@users.noreply.github.com>
2026-04-28 15:09:01 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -66,8 +66,7 @@ function short(text, max = 24) {
 async function getSheetsClient() {
   const auth = new google.auth.JWT({
     email: process.env.GOOGLE_CLIENT_EMAIL,
-    key: process.env.GOOGLE_PRIVATE_KEY.replace(/\n/g, "
-"),
+    key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
     scopes: ["https://www.googleapis.com/auth/spreadsheets"],
   });
 
